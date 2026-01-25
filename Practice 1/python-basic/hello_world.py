2ff9571c8756814f4a4b70b5ffe4ab03a1030e02
Your Name <your.email@example.com>
2026-01-25 11:55:25 +0500

    adding 1 file

---
+++ Practice 1/python-basic/hello_world.py
@@ -0,0 +1,30 @@
+#1
+print("Hello, World!")
+
+#2
+#This is a comment.
+print("Hello, World!") #This is a comment
+#This is a comment
+#written in
+#more than just one line
+"""
+This is a comment
+written in
+more than just one line
+"""
+
+#3
+print("This will work!")
+print('This will also work!')
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+        
+#4
+a = 1440
+b = "Hello, World!"
+
+#5
+print("Hello, World!") 
+print("Ura, KBTU")
+print("some example")
+print("Hello"); print("How are you?"); print("Bye bye!")
---
+++ Practice 1/python-basic/strings.py
@@ -0,0 +1,27 @@
+#1
+a="He's the imposter"
+print(a)
+print("His name is'Freddy'")
+print('His name is "Freddy"')
+b = """To be, or not to be, that is the question (Hamlet),
+All the world's a stage (As You Like It),
+Some are born great, some achieve greatness, and some have greatness thrust upon them (Twelfth Night)."""
+print(b)
+print(b[1])
+print(len(b))
+for x in "Mama":
+    print(x)
+if "like" in b:
+  print("Yes, I 'like' it.")
+print("love" not in b)
+
+#2
+a = "I wanna be your vacuum cleaner"
+print(a[2:7])
+print(a[:7])
+print(a[11:])
+print(a[-7:-2])
+print(a.upper())
+print(a.lower())
+
+#3
